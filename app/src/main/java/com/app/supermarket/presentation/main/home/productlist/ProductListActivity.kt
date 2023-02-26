798d086cea12a867e92a4e520e0880a5f7eba6d9
moamen <moamen.ahmed@shgardi.app>
2023-02-26 23:29:56 +0200

    product list

---
+++ app/src/main/java/com/app/supermarket/presentation/main/home/productlist/ProductListActivity.kt
@@ -0,0 +1,12 @@
+package com.app.supermarket.presentation.main.home.productlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.supermarket.R
+
+class ProductListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_list)
+    }
+}
