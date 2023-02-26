cf6a2512b8ab378b8a89d8610100b65f35872e9e
moamen <moamen.ahmed@shgardi.app>
2023-02-27 01:10:07 +0200

    product list

--- app/src/main/java/com/app/supermarket/presentation/main/MainActivity.kt
+++ app/src/main/java/com/app/supermarket/presentation/main/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.app.supermarket.presentation.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.app.supermarket.R
--- app/src/main/java/com/app/supermarket/presentation/main/home/productlist/SingleProductFragment.kt
+++ app/src/main/java/com/app/supermarket/presentation/main/home/productlist/SingleProductFragment.kt
@@ -6,55 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.app.supermarket.R
+import com.app.supermarket.base.BaseFragment
+import com.app.supermarket.databinding.FragmentSingleProductBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class SingleProductFragment : BaseFragment<FragmentSingleProductBinding>() {
+    override val layoutRes: Int
+        get() = R.layout.fragment_single_product
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SingleProductFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SingleProductFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_single_product, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SingleProductFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SingleProductFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun initUI(savedInstanceState: Bundle?) {
     }
 }
