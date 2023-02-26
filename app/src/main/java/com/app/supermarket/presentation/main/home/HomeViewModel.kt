cca130dd52086507c0b3e839b3a236d5b59e3268
Ahmedbadr22 <ahmedbadr200121@gmail.com>
2023-02-27 00:10:07 +0200

    feature: add search functionality with some bugs

--- app/src/main/java/com/app/supermarket/presentation/main/home/HomeViewModel.kt
+++ app/src/main/java/com/app/supermarket/presentation/main/home/HomeViewModel.kt
@@ -1,10 +1,13 @@
 package com.app.supermarket.presentation.main.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.app.supermarket.base.BaseResponse
 import com.app.supermarket.base.Resource
 import com.app.supermarket.data.models.response.CategoryHomeResponse
+import com.app.supermarket.data.models.response.Item
 import com.app.supermarket.domain.usecase.CategoryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,6 +21,10 @@ class HomeViewModel @Inject constructor(
     private val categoryUseCase: CategoryUseCase,
 ) : ViewModel() {
 
+    private val _categoriesLiveData : MutableLiveData<List<Item>> = MutableLiveData()
+    val categoriesLiveData : LiveData<List<Item>>
+        get() = _categoriesLiveData
+
     private val _categoryStateFlow = MutableStateFlow<Resource<BaseResponse<CategoryHomeResponse>>>(Resource.Default)
     val categoryStateFlow: StateFlow<Resource<BaseResponse<CategoryHomeResponse>>> = _categoryStateFlow
 
@@ -27,9 +34,20 @@ class HomeViewModel @Inject constructor(
 
     private fun getAllCategory(){
         viewModelScope.launch {
-            categoryUseCase.invoke().collectLatest {
-                _categoryStateFlow.value = it
+            categoryUseCase.invoke().collectLatest { resource ->
+                _categoryStateFlow.value = resource
+                if (resource is Resource.Success) {
+                    _categoriesLiveData.value = resource.value.result.items
+                }
             }
         }
+    }
+
+    fun searchCategories(value: String) {
+        val data = _categoriesLiveData.value ?: emptyList()
+        val searchResult = data.filter { item ->
+            item.title.contains(value)
+        }
+        _categoriesLiveData.value = searchResult
     }
 }
