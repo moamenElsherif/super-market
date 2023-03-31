acc1b2b92b8ca0f53e4061701da78b24698122d0
moamen <moamen.ahmed@shgardi.app>
2023-04-01 00:37:15 +0200

    add access token to header

--- app/src/main/java/com/app/supermarket/base/BaseApplication.kt
+++ app/src/main/java/com/app/supermarket/base/BaseApplication.kt
@@ -1,17 +1,32 @@
 package com.app.supermarket.base
 
 import android.app.Application
+import com.app.supermarket.base.auth.Auth
 import com.app.supermarket.base.pref.AppPref
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.Forest.plant
+import javax.inject.Inject
 
 
 @HiltAndroidApp
 class BaseApplication : Application() {
+
+    @Inject
+    lateinit var auth: Auth
+
     override fun onCreate() {
         super.onCreate()
         plant(Timber.DebugTree())
         AppPref.init(applicationContext)
+        instance = this
+    }
+
+
+    companion object {
+        lateinit var instance: BaseApplication
+        fun getAppInstance(): BaseApplication {
+            return instance
+        }
     }
 }
